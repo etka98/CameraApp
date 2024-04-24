a6c2b24546ca5be2628ad971a5b449fb1f2506b6
etkauzun <etka.uzun@getir.com>
2024-04-24 23:04:27 +0300

    Add image capturing functionality

--- CameraApp/Extension/UIView+Extension.swift
+++ CameraApp/Extension/UIView+Extension.swift
@@ -33,4 +33,13 @@ extension UIView {
             centerYAnchor.constraint(equalTo: subview.centerYAnchor)
         ])
     }
+    
+    func set(width: CGFloat? = nil, height: CGFloat? = nil) {
+        if let width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        if let height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
 }
